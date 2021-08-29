bae218b6e03d87c63ee5e66ffe1b24364bd7cff0
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2021-08-29 14:57:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from heroku3 import from_key
 from pyrogram import Client
 
-API_ID = int(os.environ.get("API_ID", "6"))
+API_ID = int(os.environ.get("API_ID", "7717799"))
 API_HASH = os.environ.get("API_HASH", "d6e0095d9f089d956ec3c298ca0471ba")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 ARQ_API_KEY = "MSZAYQ-IYVZRT-QELHBM-FISUKY-ARQ" 
